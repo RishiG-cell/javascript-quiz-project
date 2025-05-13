80f82bf3402e66f7a03434fbab73f451278986e4
Rishi G. <mail.rishig@gmail.com>
2025-05-13 17:32:58 +0200

    update quiz

--- src/quiz.js
+++ src/quiz.js
@@ -1,47 +1,59 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-     constructor (questions, timeLimit, timeRemaining){
-        this.questions = questions ;
-        this.timeLimit = timeLimit ;
-        this.timeRemaining = timeRemaining ;
-        this.correctAnswers = 0 ;
-        this.currentQuestionIndex = 0 ;
-     }
+  // YOUR CODE HERE:
+  //
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    getQuestion(){
-        return this.questions[this.currentQuestionIndex]
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  moveToNextQuestion() {
+    return this.currentQuestionIndex++;
+  }
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
     }
-    moveToNextQuestion(){
-return this.currentQuestionIndex++
-    }
-    shuffleQuestions(){
-       for (let i = this.questions.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
-    }
-    }
+  }
 
-    checkAnswer(answer){
-        if(answer === answer){
-            this.correctAnswers++
+  checkAnswer(answer) {
+    if (answer === answer) {
+      this.correctAnswers++;
+    }
+  }
+
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    }
+  }
+  filterQuestionsByDifficulty(difficulty) {
+    if (difficulty >= 1 || difficulty <= 3) {
+      const filteredQuestions = this.questions.filter((question) => {
+        if (question.difficulty === difficulty) {
+          return true;
         }
+      });
+
+      this.questions = filteredQuestions;
     }
+  }
 
-    hasEnded(){
-        if(this.currentQuestionIndex < this.questions.length) {
-            return false
-        } else if(this.currentQuestionIndex === this.questions.length){
-            return true
-
-        }
-    }
-    filterQuestionsByDifficulty(difficulty){
-        
-
-    }
-
-    averageDifficulty(){
-
-    }
+  averageDifficulty() {
+    let total = this.questions.reduce((acc, curr) => {
+      return acc + curr.difficulty;
+    }, 0);
+    return total / this.questions.length;
+  }
 }
